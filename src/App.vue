59383bc3070329e8225c8ef4982800151a0ca743
Warren <bwarren@eab.com>
2019-01-06 18:01:54 -0500

    CRUD wired up

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-
+    <div v-if="this.editingTopic == null">
     <form @submit.prevent="createTopic">
       <div class="form-group">
         <label>New Topic</label>
@@ -8,11 +8,19 @@
       </div>
       <button>Send</button>
     </form>
-
+    </div>
     <div v-for="topic in topics" class="topic-deck">
       <div class="topic">
-        <h1>{{topic.title}}</h1>
-        <button @click="deleteTopic(topic)">Delete</button>
+        <div v-if="editingTopic === topic">
+          <input type="text" v-model="newTopic">
+          <button @click="updateTopic(topic)">Update</button>
+          <button @click="cancelUpdate(topic)">Cancel</button>
+        </div>
+        <div v-else>
+          <h1>{{topic.title}}</h1>
+          <button @click="deleteTopic(topic)">Delete</button>
+          <button @click="editTopic(topic)">Edit</button>
+        </div>
       </div>
     </div>
 
@@ -29,12 +37,12 @@
     data () {
       return {
         topics: [],
-        newTopic: ""
+        newTopic: "",
+        editingTopic: null
       }
     },
     methods: {
       createTopic(){
-        this.topics.push(this.newTopic)
         topicsRef.push({
           user: 'ben',
           title: this.newTopic
@@ -43,11 +51,33 @@
       },
       deleteTopic(topic){
         topicsRef.child(topic.id).remove()
+      },
+      editTopic(topic){
+        this.editingTopic = topic
+        this.newTopic = topic.title
+      },
+      cancelUpdate(topic){
+        this.editingTopic = null
+        this.newTopic = ""
+      },
+      updateTopic(topic){
+        topicsRef.child(this.editingTopic.id).update({title: this.newTopic})
+        this.newTopic = ""
+        this.editingTopic = null
       }
     },
     created(){
       topicsRef.on('child_added', (snapshot) => {
         this.topics.push({...snapshot.val(), id: snapshot.key})
+      })
+      topicsRef.on('child_removed', (snapshot) => {
+        const deletedTopic = this.topics.find(topic => topic.id == snapshot.key)
+        const id = this.topics.indexOf(deletedTopic)
+        this.topics.splice(id, 1)
+      }),
+      topicsRef.on('child_changed', (snapshot) => {
+        const changedTopic = this.topics.find(topic => topic.id == snapshot.key)
+        changedTopic.title = snapshot.val().title
       })
     }
   }
