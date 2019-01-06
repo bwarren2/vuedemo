472364a489ab0a6b576ed3a735b77bcd84eafa5e
Warren <bwarren@eab.com>
2019-01-06 16:32:29 -0500

    Can write and delete

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,8 @@
 
     <div v-for="topic in topics" class="topic-deck">
       <div class="topic">
-        <h1>{{topic}}</h1>
+        <h1>{{topic.title}}</h1>
+        <button @click="deleteTopic(topic)">Delete</button>
       </div>
     </div>
 
@@ -19,27 +20,37 @@
 </template>
 
 <script>
-import {db} from './firebase.js'
-export default {
-  name: 'app',
-  data () {
-    return {
-      topics: [],
-      newTopic: ""
+
+  import {db} from './firebase.js'
+  let topicsRef = db.ref('topics')
+
+  export default {
+    name: 'app',
+    data () {
+      return {
+        topics: [],
+        newTopic: ""
+      }
+    },
+    methods: {
+      createTopic(){
+        this.topics.push(this.newTopic)
+        topicsRef.push({
+          user: 'ben',
+          title: this.newTopic
+        })
+        this.newTopic = ""
+      },
+      deleteTopic(topic){
+        topicsRef.child(topic.id).remove()
+      }
+    },
+    created(){
+      topicsRef.on('child_added', (snapshot) => {
+        this.topics.push({...snapshot.val(), id: snapshot.key})
+      })
     }
-  },
-  methods: {
-    createTopic(){
-      this.topics.push(this.newTopic)
-      this.newTopic = ""
-    }
-  },
-  created(){
-    // db.ref(new Date().getFullYear()).set({
-    //   user: 'ben'
-    // })
   }
-}
 </script>
 
 <style>
